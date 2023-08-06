cb31f32a975fbe150f216e20563378e0cbf56acc
andy01609 <134120603+andy01609@users.noreply.github.com>
2023-08-05 23:01:20 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('.header');
+  header.classList.toggle('scrolled', window.scrollY > 50);
+});
+window.addEventListener('scroll', function() {
+  document.body.classList.toggle('scrolled', window.scrollY > 50);
+});
+
+window.addEventListener('scroll', function() {
+  document.body.classList.toggle('scrolled', window.scrollY > 50);
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+var navbarLinks = document.querySelectorAll('.navbar-link');
+
+// Add a click event listener to each link
+navbarLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    // When a link is clicked, first remove the active class from all links
+    navbarLinks.forEach(function(innerLink) {
+      innerLink.classList.remove('active');
+    });
+
+    // Then add the active class to the clicked link
+    this.classList.add('active');
+  });
+});
